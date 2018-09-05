d743be3cc5ed31ee86d4ab5f3de51ef47a21ad54
Kyire24 <wujiechoupp@sina.cn>
2018-09-05 12:02:01 +0800

    First Submit

---
+++ service-feign/src/main/java/com/forezp/servicefeign/clients/SchedualServiceHi.java
@@ -0,0 +1,19 @@
+package com.forezp.servicefeign.clients;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * Created by Administrator on 2018/8/31 0031.
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+}
